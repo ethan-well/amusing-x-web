3f571e6228cbc5c78f7269aa7f953358e24269ae
魏巍 <wewin11235@163.com>
2022-05-30 08:21:46 +0800

    request sub product pictures

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,16 +8,13 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Image1 from "./image1.jpeg";
 import { red } from "@material-ui/core/colors";
-import Tooltip from "@material-ui/core/Tooltip";
-import Button from "@material-ui/core/Button";
-import clsx from "clsx";
 import IconButton from "@material-ui/core/IconButton";
 import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
 import AddIcon from "@material-ui/icons/Add";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { green } from '@material-ui/core/colors';
-import Icon from '@material-ui/core/Icon';
+import RemoveIcon from "@material-ui/icons/Remove";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { RequestData } from "../account/submit";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,11 +36,50 @@ const useStyles = makeStyles((theme) => ({
       duration: theme.transitions.duration.shortest,
     }),
   },
+  price: {},
 }));
 
 function ImgMediaCard(props) {
   const classes = useStyles();
   const subProduct = JSON.parse(props.subProductStr);
+  const [count, setCount] = useState(1);
+  const [showTip, setShowTip] = useState(false);
+  const [tip, setTip] = useState("");
+  const [pictures, setPictures] = useState({});
+
+  const addCount = function () {
+    if (count + 1 > subProduct.subProductInfo.max_num) {
+      setTip("超过最大可售卖数量限制！");
+      setShowTip(true);
+    } else {
+      setCount(count + 1);
+    }
+  };
+
+  const removeCount = function () {
+    if (count - 1 < subProduct.subProductInfo.min_num) {
+      setTip("数量小于最小售卖数量限制！");
+      setShowTip(true);
+    } else {
+      setCount(count - 1);
+    }
+  };
+
+  const getProductsCallback = (data) => {
+    console.log("data: ", data);
+    if (data && data.result && data.result.sub_product_pictures) {
+      setPictures(data.result.sub_product_pictures[0].pictures);
+    }
+  };
+
+  useEffect(() => {
+    if (subProduct.subProductInfo.id >= 1) {
+      RequestData(
+        `v1/europa/product/sub_product/pictures?sub_product_ids=9,11`,
+        getProductsCallback
+      );
+    }
+  }, []);
 
   return (
     <Card>
@@ -52,7 +88,8 @@ function ImgMediaCard(props) {
           component="img"
           alt="Contemplative Reptile"
           height="200"
-          image={Image1}
+          // image={Image1}
+          src={pictures[0].src}
           title="Contemplative Reptile"
         />
         <CardContent>
@@ -70,23 +107,29 @@ function ImgMediaCard(props) {
         </CardContent>
       </CardActionArea>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <AddIcon />
-        </IconButton>
-        <IconButton className={classes.expand}>
-          <ExpandMoreIcon />
-        </IconButton>
+        <Grid container spacing={3}>
+          <Grid container item xs={3} alignContent="center">
+            <span class="currency">
+              {subProduct.subProductInfo.currencySymbol}
+            </span>
+            <span class="value">{subProduct.subProductInfo.price}</span>
+          </Grid>
+          <Grid item xs={6}>
+            <IconButton aria-label="remove">
+              <RemoveIcon onClick={() => removeCount()} />
+            </IconButton>
+            <span>{count}</span>
+            <IconButton aria-label="add">
+              <AddIcon onClick={() => addCount()} />
+            </IconButton>
+          </Grid>
+          <Grid item container alignContent="center" xs={3}>
+            <Button variant="outlined" color="secondary">
+              购买
+            </Button>
+          </Grid>
+        </Grid>
       </CardActions>
-
-      {/* <CardActions disableSpacing={true}> */}
-      {/* {subProduct.subProductInfo.price} */}
-      {/* <Button variant="contained" color="secondary" className={classes.expand}> */}
-      {/* 购买 */}
-      {/* </Button> */}
-      {/* </CardActions> */}
     </Card>
   );
 }
